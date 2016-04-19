2e890563f2b68b888e6c7aad54da15f9cda37815
SirEdvin <siredvin.dark@gmail.com>
2016-04-19 17:21:48 +0300

    [IMP] Add read_group_mixin experimental module

---
+++ read_group_mixins/__openerp__.py
@@ -0,0 +1,20 @@
+#-*- coding:utf-8 -*-
+{
+    'name': 'Read Group Mixins',
+    'version': '0.1',
+    'summary': 'Read Group mixins',
+    'category': 'Tools',
+    'author': 'KIT-XXI',
+    'website': "http://kit-xxi.com.ua/",
+    'description':
+        """
+Custom colored widgets
+======================
+
+A collection of mixins models for read_group method:
+- To manual process group list
+- To add computed field to result
+        """,
+    'data': [
+    ]
+}
